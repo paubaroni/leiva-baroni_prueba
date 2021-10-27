ba256d279b5bb6acee49be67d225681f20d23456
paubaroni <92754565+paubaroni@users.noreply.github.com>
2021-10-27 16:49:16 -0300

    Add files via upload

---
+++ baroni,leiva_prueba.py
@@ -0,0 +1,16 @@
+# Get user email address
+email = input("What is your email address?: ").strip()
+
+# Slice out the user name
+user_name = email[:email.index("@")]
+
+email = input("what is your password?: ").strip()
+
+# Slice the domain name
+domain_name = email[email.index("@")+1:]
+
+# Format message
+output = "Your username is '{}' and your domain name is '{}'".format(user_name,domain_name)
+
+# Display output message
+print(output)
